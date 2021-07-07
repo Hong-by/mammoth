85e9bbf3b24bf38eb69439964ea44341d853ec44
HongBY <hby033@naver.com>
2021-07-07 18:22:35 +0900

    업데이트: main page 2번째 section까지 완료 / Fullpage Plugin 적용 완료 / 2번째 section 정렬 틀어짐 수정 필요

--- js/jq.main.js
+++ js/jq.main.js
@@ -1,17 +1,17 @@
 $(function(){
-  // Mega Box Menu Show When Hovering Navigation
-  $(".topnav, .hidden_sub").on("mouseenter",function(){
+  // Mega Box Menu Show and Hide When Hovering Navigation
+  $(".topnav, .hidden_sub").on("mouseenter", function(){
     $(".hidden_sub").css({display:"block"});
     $(".topnav li a").css({color:"#000"});
     $(".hidden_sub").stop().animate({height:"485px"}, 300);
   });
 
-  $(".topnav, .hidden_sub").on("mouseleave",function(){
-    $(".hidden_sub").stop().animate({height:"0"}, 300, function(){
+  $(".topnav, .hidden_sub").on("mouseleave", function(){
+    $(".hidden_sub").stop().animate({height:0}, 300, function(){
       $(".topnav li a").css({color:"#fff"});
       $(".hidden_sub").css({display:"none"});
-    });
-  });
+    });   
+  });   
 
   // Sub Mega Box Menu Hide and Show
   $(".topnav li").on("mouseenter", function(){
@@ -31,41 +31,38 @@ $(function(){
     $("section").animate({left:'60%'}, 300);
     $(".overlay").animate({
       "opacity":0.7,
-      "left":'60%',
+      "left": "60%",
       "width":"100%"
-    }, 300)
-  });
+    }, 300);
+  });   
 
-  $(".close_btn").click(function(){
+  $(".close_btn, .overlay").click(function(){
     $(".mobile_nav").animate({left:'-60%'}, 300);
     $("section").animate({left:0}, 300);
     $(".overlay").animate({
       "opacity":0,
-      "left":0,
+      "left": 0,
       "width":0
-    }, 300)
+    }, 300);
   });
 
   // Mobile Menu Accordion Effect
   $(".mobile_tit").click(function(){
     if($(this).hasClass("active")){
       $(this).removeClass("active");
-      $(this).siblings("mobile_sub_nav").slideUp(500);
+      $(this).siblings(".mobile_sub_nav").slideUp(200);
       $(this).removeClass("rotate");
       $(".mobile_tit").removeClass("rotate");
     } else {
       $(".mobile_tit").removeClass("active");
       $(".mobile_tit").removeClass("rotate");
-      $(this).addClass("rotate");
       $(this).addClass("active");
-      $("mobile_sub_nav").slideUp(500);
-      $(this).siblings("mobile_sub_nav").slideDown(500);
-      
-
+      $(".mobile_sub_nav").slideUp(200);
+      $(this).siblings(".mobile_sub_nav").slideDown(200);
+      $(this).addClass("rotate");   
     }
   });
-
-
+  
 });
 
 // $(document).ready(function(){});
